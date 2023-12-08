f4f73da0d3bc69be1344cef708e85ffc74ddf29a
Geraldo-git <laboratorio.inicial@gmail.com>
2023-12-08 10:27:56 -0300

    classes Nokia, Iphone e testes

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,8 +4,8 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-        public string Modelo { get; set; }
-        public string IMEI { get; set; }
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
         private int Memoria { get; set; }
 
         public Smartphone(string numero, string modelo, string imei, int memoria)
--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using DesafioPOO.Models;
+
+Nokia nokia = new Nokia("1111-2222", "Lumia", "01ab01235688", 8);
+Iphone iphone = new Iphone("3333-4444", "Iphone 12", "01zx01185688", 8);
+
+Console.WriteLine("================================================");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+Console.WriteLine("================================================");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
+Console.WriteLine("================================================");
